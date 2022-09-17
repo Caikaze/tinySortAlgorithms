3693c9e0bbac5e8bd895f1037f66bb1176ae611c
Cmz <kaze97sleigh@gmail.com>
2022-09-17 11:29:03 +0800

    fix bubblesort and add illustration

--- src/bubbleSort.cpp
+++ src/bubbleSort.cpp
@@ -1,12 +1,16 @@
+/*
+冒泡排序(升序)的基本思想是每次都从头开始两两比较相邻的元素，将较大的元素换到后面
+因此，每次都会有一个元素达到最终的排序位置，根据这个思路实现了如下代码
+*/
 #include"bubbleSort.h"
 
 void bubbleSort(vector<int> &arr)
 {
     int len = arr.size();
     for (int i = 0; i < len; ++i) {
-        for (int j = i + 1; j < len; ++j) {
-            if (arr[i] > arr[j]) {
-                swap(arr[i], arr[j]);
+        for (int j = 0; j < len - 1 - i; ++j) {
+            if (arr[j + 1] > arr[j]) {
+                swap(arr[j + 1], arr[j]);
             }
         }
     }
